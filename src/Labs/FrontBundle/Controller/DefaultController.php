c00fa9f5da5616c86349744a210acba15cdef872
raphael sampan <raphaelsampan@gmail.com>
2016-12-13 12:42:09 +0000

    retour en arriere pour les pages sections et items en page d'accueil

--- src/Labs/FrontBundle/Controller/DefaultController.php
+++ src/Labs/FrontBundle/Controller/DefaultController.php
@@ -2,11 +2,10 @@
 
 namespace Labs\FrontBundle\Controller;
 
-use Labs\AdminBundle\Entity\Item;
-use Labs\AdminBundle\Entity\Section;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -20,45 +19,15 @@ class DefaultController extends Controller
     }
 
     /**
-     * @param Section $section
      * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
-     * @Route("/{slug}", name="section_front_view")
-     * @Method({"GET"})
-     */
-    public function ViewSectionAction(Section $section)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $sections = $em->getRepository('LabsAdminBundle:Section')->getSectionAllPosts($section);
-        return $this->render('LabsFrontBundle:Sections:view_section.html.twig',[
-            'sections' => $sections
-        ]);
-    }
-
-    /**
-     * @param Item $item
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
-     * @Route("/{rubrique}/{slug}", name="item_front_view")
-     * @Method({"GET"})
-     */
-    public function ViewItemAction(Item $item)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $items = $em->getRepository('LabsAdminBundle:Item')->getItemAllPosts($item);
-        return $this->render('LabsFrontBundle:Items:view_item.html.twig',[
-            'items' => $items
-        ]);
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
      */
     public function getMenuAction()
     {
         $em = $this->getDoctrine()->getManager();
         $sections = $em->getRepository('LabsAdminBundle:Section')->getSectionsAndItems();
+        if(null === $sections){
+            throw  new NotFoundHttpException('Element introuvable');
+        }
         return $this->render('LabsFrontBundle:Includes:header.html.twig',[
             'sections' => $sections
         ]);
