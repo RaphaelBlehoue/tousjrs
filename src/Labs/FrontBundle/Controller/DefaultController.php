7019d050d783beca12bec229e054a5c4a19a1af7
raphael sampan <raphaelsampan@gmail.com>
2016-12-13 13:52:58 +0000

    Mise en place des Menus et page section  et item

--- src/Labs/FrontBundle/Controller/DefaultController.php
+++ src/Labs/FrontBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace Labs\FrontBundle\Controller;
 
+use Labs\AdminBundle\Entity\Item;
+use Labs\AdminBundle\Entity\Section;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -30,6 +32,42 @@ class DefaultController extends Controller
         }
         return $this->render('LabsFrontBundle:Includes:header.html.twig',[
             'sections' => $sections
+        ]);
+    }
+
+    /**
+     * @param Section $section
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/tj/{slug}", name="front_section_page")
+     * @Method({"GET"})
+     */
+    public function getPageSectionAction(Section $section)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $sections = $em->getRepository('LabsAdminBundle:Section')->getSectionAllPosts($section);
+        if(null === $sections){
+            throw new NotFoundHttpException('Page introuvable');
+        }
+        return $this->render('LabsFrontBundle:Sections:view_section.html.twig',[
+            'sections' => $sections
+        ]);
+    }
+
+    /**
+     * @param Item $item
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/{rubrique}/p/{slug}", name="front_item_page")
+     * @Method({"GET"})
+     */
+    public function getPageItemAction(Item $item)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $items = $em->getRepository('LabsAdminBundle:Item')->getItemAllPosts($item);
+        if(null === $items){
+            throw new NotFoundHttpException('Page introuvable');
+        }
+        return $this->render('LabsFrontBundle:Items:view_item.html.twig',[
+            'items' => $items
         ]);
     }
 }
