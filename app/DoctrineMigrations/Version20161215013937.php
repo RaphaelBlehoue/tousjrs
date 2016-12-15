1c62b1e7360bb22d06251eb8d8fd26368d814396
raphael sampan <raphaelsampan@gmail.com>
2016-12-15 01:40:56 +0000

    Suppression des migration d'erreur

--- app/DoctrineMigrations/Version20161215013937.php
+++ app/DoctrineMigrations/Version20161215013937.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20161213142211 extends AbstractMigration
+class Version20161215013937 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,12 +18,11 @@ class Version20161213142211 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE media DROP FOREIGN KEY FK_6A2CA10C4B89032C');
-        $this->addSql('ALTER TABLE media ADD created DATETIME NOT NULL');
+        $this->addSql('CREATE TABLE media (id INT AUTO_INCREMENT NOT NULL, post_id INT DEFAULT NULL, url VARCHAR(255) NOT NULL, status TINYINT(1) NOT NULL, created DATETIME NOT NULL, actived TINYINT(1) NOT NULL, INDEX IDX_6A2CA10C4B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, item_id INT DEFAULT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, content LONGTEXT DEFAULT NULL, online TINYINT(1) DEFAULT NULL, draft TINYINT(1) DEFAULT NULL, created DATETIME NOT NULL, has_video TINYINT(1) DEFAULT NULL, INDEX IDX_5A8A6C8D126F525E (item_id), INDEX IDX_5A8A6C8DA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10C4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
-        $this->addSql('ALTER TABLE post ADD item_id INT DEFAULT NULL, ADD has_video TINYINT(1) NOT NULL, CHANGE name name VARCHAR(255) NOT NULL, CHANGE content content LONGTEXT NOT NULL, CHANGE created created DATETIME NOT NULL');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D126F525E FOREIGN KEY (item_id) REFERENCES item (id)');
-        $this->addSql('CREATE INDEX IDX_5A8A6C8D126F525E ON post (item_id)');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
     }
 
     /**
@@ -35,10 +34,7 @@ class Version20161213142211 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE media DROP FOREIGN KEY FK_6A2CA10C4B89032C');
-        $this->addSql('ALTER TABLE media DROP created');
-        $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10C4B89032C FOREIGN KEY (post_id) REFERENCES media (id)');
-        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D126F525E');
-        $this->addSql('DROP INDEX IDX_5A8A6C8D126F525E ON post');
-        $this->addSql('ALTER TABLE post DROP item_id, DROP has_video, CHANGE name name VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, CHANGE content content LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci, CHANGE created created DATETIME DEFAULT NULL');
+        $this->addSql('DROP TABLE media');
+        $this->addSql('DROP TABLE post');
     }
 }
